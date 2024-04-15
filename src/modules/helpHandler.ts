265d879dbe764f41e30cca0c531a3e9cedc0dbb7
mtoxas <mtoxas@gmail.com>
2024-04-15 10:21:37 +0300

    refactor help

--- src/modules/helpHandler.ts
+++ src/modules/helpHandler.ts
@@ -10,16 +10,22 @@ Arguments:
   project-name    Name of the project/project directory (optional)
 
 Options:
-  --markup        Choose template engine:
-                    'html' | 'pug' | 'ejs' | 'handlebars'
-  --style         Choose style processing tool:
-                    'css' | 'scss' | 'sass' | 'stylus' | 'less' | 'tailwind'
-  --script        Choose JavaScript tool:
-                    'javascript' | 'typescript' | 'jquery'
-  --eslint        Add ESLint
-  --stylelint     Add Stylelint
-  --prettier      Add Prettier
-  --help, --h     Display this help message
+  --markup          Choose template engine:
+                     'html' | 'pug' | 'ejs' | 'handlebars'
+  --style           Choose style processing tool:
+                      'css' | 'scss' | 'sass' | 'stylus' | 'less' | 'tailwind'
+  --script          Choose JavaScript tool:
+                      'javascript' | 'typescript' | 'jquery'
+  --eslint         Add ESLint
+  --stylelint      Add Stylelint
+  --prettier       Add Prettier
+  --help, --h       Display this help message
+
+Examples:
+npm create parcel-static
+npm create parcel-static my-static-app
+npm create parcel-static my-static-app -- --markup pug --script javascript --eslint 
+yarn create parcel-static my-static-app --style scss --markup html
 
 📦 Happy hacking! 
     `);
